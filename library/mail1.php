55574ea01a014c3b0ed3d2a6054ca5f2161d2fa3
pudimKBM <pudimkbm@gmail.com>
2018-08-06 16:38:41 -0300

    mudanças no mailer
    mucança no mailer com php mialer

--- library/mail1.php
+++ library/mail1.php
@@ -1,11 +1,47 @@
 <?php
-    ini_set( 'display_errors', 1 );
-    error_reporting( E_ALL );
-    $from = "adm@adaptspace.com.br";
-    $to = "adm@adaptspace.com.br";
-    $subject = "Checking PHP mail";
-    $message = "PHP mail works just fine";
-    $headers = "From:" . $from;
-    mail($to,$subject,$message, $headers);
-    echo "The email message was sent.";
+    use PHPMailer\PHPMailer\PHPMailer;
+
+    require "../vendor/autoload.php";
+    $mail = new PHPMailer();
+    $mail->isSMTP();
+    $mail->Host = "mx1.hostinger.com.br";
+    $mail->Port  = 587;
+    $mail->SMTPSecure= 'tls';
+    $mail->SMTPAuth = true;
+    $mail->Username = "adm@adaptspace.com.br";
+    $mail->Password ="J3@ta@ti27";
+
+
+    $mail->setFrom('adm@adaptspace.com.br','AdaptSpace');
+    $mail->addAddress('adm@adaptspace.com.br', 'AdaptSpace');
+    $mail->Subject('Bem vido a AdaptSpace');
+    $mail->msgHTML('
+    <html>
+    <head>
+    <title>HTML email</title>
+    </head>
+    <body>
+    <p>This email contains HTML Tags!</p>
+    <table>
+    <tr>
+    <th>Firstname</th>
+    <th>Lastname</th>
+    </tr>
+    <tr>
+    <td>John</td>
+    <td>Doe</td>
+    </tr>
+    </table>
+    </body>
+    </html>');
+    $mail->AltBody('ola @ meu gamigo como vai ');
+    
+    if (!$mail->send()){
+        echo "erro" . $mail->ErrorInfo();
+
+    }else{
+        echo "message sent";
+
+    }
+
 ?>
