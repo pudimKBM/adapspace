58c8f766c4e3ecc97ee1ff23d718666b6c67a6b7
pudimKBM <pudimkbm@gmail.com>
2018-08-07 11:01:07 -0300

    criaçao de arquivo de configuraçao de e-mail

--- library/mail1.php
+++ library/mail1.php
@@ -1,18 +1,7 @@
 <?php
-    use PHPMailer\PHPMailer\PHPMailer;
+      include 'comfig_mail.php';
 
-    require "vendor/autoload.php";
-    $mail = new PHPMailer();
-    $mail->isSMTP();
-    $mail->Host = "mx1.hostinger.com.br";
-    $mail->Port  = 587;
-    $mail->SMTPSecure= 'tls';
-    $mail->SMTPAuth = true;
-    $mail->Username = "adm@adaptspace.com.br";
-    $mail->Password ="J3@ta@ti27";
-
-
-    $mail->setFrom('adm@adaptspace.com.br','AdaptSpace');
+      
     $mail->addAddress("$email", "$firstname");
     $mail->Subject = 'Bem-vindo à Adapt Space';
     $mail->msgHTML("
--- library/passrecovery.php
+++ library/passrecovery.php
@@ -1,18 +1,5 @@
 <?php
-    use PHPMailer\PHPMailer\PHPMailer;
-
-    require "vendor/autoload.php";
-    $mail = new PHPMailer();
-    $mail->isSMTP();
-    $mail->Host = "mx1.hostinger.com.br";
-    $mail->Port  = 587;
-    $mail->SMTPSecure= 'tls';
-    $mail->SMTPAuth = true;
-    $mail->Username = "adm@adaptspace.com.br";
-    $mail->Password ="J3@ta@ti27";
-
-
-    $mail->setFrom('adm@adaptspace.com.br','AdaptSpace');
+      include 'comfig_mail.php';
 
     $mail->addAddress("{$email}", "{$firstname}");
     $mail->addBCC("adm@adaptspace.com.br", "AdaptSpace");
