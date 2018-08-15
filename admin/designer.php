60e895d1002f23c49201c80945ded508efe8d338
pudimKBM <pudimkbm@gmail.com>
2018-08-15 15:10:08 -0300

    dasdas
    dasdasd

--- admin/designer.php
+++ admin/designer.php
@@ -164,12 +164,6 @@ collor: black;
 </head>
 
 <?php
-session_start();
-ini_set('display_errors',1);
-ini_set('display_startup_erros',1);
-error_reporting(E_ALL);
-
-
 if (! isset($_SESSION['logged_in'])) {
 	echo "<meta http-equiv='refresh' content='0;url=http://adaptspace.com.br/index.php' />";
     
@@ -179,6 +173,13 @@ if (! isset($_SESSION['logged_in'])) {
     $name_category = $_GET['category'];
     $caticon = $_GET['icon'];
     
+	session_start();
+ini_set('display_errors',1);
+ini_set('display_startup_erros',1);
+error_reporting(E_ALL);
+
+
+
     ?>
 
 
