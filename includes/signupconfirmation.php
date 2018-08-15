a1a0b9e243683ae767a2e656849876d33d283f04
pudimKBM <pudimkbm@gmail.com>
2018-08-15 14:43:10 -0300

    dasda
    asdasda

--- includes/signupconfirmation.php
+++ includes/signupconfirmation.php
@@ -20,7 +20,7 @@ error_reporting(E_ALL);
         
         // connecting & inserting data
         // VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6])
-        $query = "INSERT INTO `users`( `email`, `firstname`, `lastname`, `password`, `role`,'rec_date') VALUES (
+        $query = "INSERT INTO `users`( `email`, `firstname`, `lastname`, `password`, `role`, rec_date) VALUES (
 '$email',
 '$firstname',
 '$lastname',
