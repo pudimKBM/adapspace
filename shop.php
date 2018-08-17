b9d3eafd6a12711e1e90fe29e108cb0f6b4bf845
BiaHer <biaher.oliveira@gmail.com>
2018-08-17 16:17:11 -0300

    header and section margin removed

--- shop.php
+++ shop.php
@@ -83,7 +83,7 @@ if ($_SESSION['logged_in']) {
 			<div class="row">
 				<div class="col-sm-6 col-md-4 col-lg-3 p-b-50">
 					<div class="leftbar p-r-20 p-r-0-sm">
-						<!--  -->
+						<!--  
 						<h4 class="m-text14 p-b-7">Categorias</h4>
 
 						<ul class="p-b-54">
@@ -93,7 +93,7 @@ if ($_SESSION['logged_in']) {
 							<li class="p-t-4"><a  class="s-text13">Influencers </a></li>
 						</ul>
 
-						<!--  -->
+						  
 
 
 						<div class="search-product pos-relative bo4 of-hidden">
@@ -104,6 +104,7 @@ if ($_SESSION['logged_in']) {
 								<i class="fs-12 fa fa-search" aria-hidden="true"></i>
 							</button>
 						</div>
+						-->
 					</div>
 				</div>
 
