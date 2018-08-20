2baffdbb99c7c89d6b70e8ebf8abd1fe581dcaf4
pudimKBM <pudimkbm@gmail.com>
2018-08-20 10:53:47 -0300

    xx
    xxx

--- js/ista.js
+++ js/ista.js
@@ -5,8 +5,8 @@
         tagName: 'doggies',
         clientId: 'd780b8cd6adc4a64ba8b3c3689bf14f4',
         userId:'175020448',
-        accessToken: '2e6569f601e6466ea432f3011003115e',
-        template: '<div class="block4 wrap-pic-w"><img src="{{image}}" alt"IMG-INSTAGRAM> <a href="{{https://www.instagram.com/adaptspace/}}" class="block4-overlay sizefull ab-t-l trans-0-4"><span class="block4-overlay-heart s-text9 flex-m trans-0-4 p-l-40 p-t-25"></a></div> ' 
+        accessToken: '4412847056.1677ed0.3c3a3acf59b34db88db08c2c8a4507cf',
+        template: '<div class="block4 wrap-pic-w"><img src="{{image}}" alt"IMG-INSTAGRAM> <a href="{{link}}" class="block4-overlay sizefull ab-t-l trans-0-4"><span class="block4-overlay-heart s-text9 flex-m trans-0-4 p-l-40 p-t-25"></a></div> ' 
     });
     feed.run();
    
