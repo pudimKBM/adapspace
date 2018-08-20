0645d193804e38f18e707ad4d7878863825c6c6c
pudimKBM <pudimkbm@gmail.com>
2018-08-20 10:32:03 -0300

    secret

--- js/ista.js
+++ js/ista.js
@@ -5,6 +5,7 @@
         tagName: 'doggies',
         clientId: '2117c2522df24eada7f6149953739100',
         userId:'7290986099',
+        accessToken:'b73c48810b184199aebc203857ccdb8a' ,
         
         template: '<div class="block4 wrap-pic-w"><img src="{{image}}" alt"IMG-INSTAGRAM> <a href="{{https://www.instagram.com/adaptspace/}}" class="block4-overlay sizefull ab-t-l trans-0-4"><span class="block4-overlay-heart s-text9 flex-m trans-0-4 p-l-40 p-t-25"></a></div> ' 
         
