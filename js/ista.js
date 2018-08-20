b95576e3407f62bd028782451a760c38307cd28f
pudimKBM <pudimkbm@gmail.com>
2018-08-20 10:41:49 -0300

    xx
    xx

--- js/ista.js
+++ js/ista.js
@@ -3,9 +3,9 @@
     var feed = new Instafeed({
         get: 'user',
         tagName: 'doggies',
-        clientId: '2117c2522df24eada7f6149953739100',
+        clientId: 'd780b8cd6adc4a64ba8b3c3689bf14f4',
         userId:'175020448',
-        accessToken: '343958669.2117c25.08b6ab9b43b042f7ab2b2aec4c6b46bd',
+        accessToken: '2e6569f601e6466ea432f3011003115e',
         template: '<div class="block4 wrap-pic-w"><img src="{{image}}" alt"IMG-INSTAGRAM> <a href="{{https://www.instagram.com/adaptspace/}}" class="block4-overlay sizefull ab-t-l trans-0-4"><span class="block4-overlay-heart s-text9 flex-m trans-0-4 p-l-40 p-t-25"></a></div> ' 
     });
     feed.run();
