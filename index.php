d16c5ffab59f704baef06eb705dfb76232ab77a1
BiaHer <biaher.oliveira@gmail.com>
2018-08-21 16:35:46 -0300

    add Ebano

--- index.php
+++ index.php
@@ -58,7 +58,7 @@
             <h2 class="text-center "><span class="font-in">Campanhas</span></h2>
             <div class="row pt-5">
                 <!-- Team member -->
-                <div class="col-xs-12 col-sm-6 col-md-4" style="visibility: hidden">
+                <!-- <div class="col-xs-12 col-sm-6 col-md-4" style="visibility: hidden">
                     <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
                         <div class="mainflip">
                             <div class="frontside">
@@ -96,9 +96,9 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> -->
 
-                <div class="col-xs-12 col-sm-6 col-md-4">
+                <div class="col-sm-6 col-md-6">
                     <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
                         <div class="mainflip">
                             <div class="frontside">
@@ -119,8 +119,8 @@
                             <div class="backside">
                                 <div class="card">
                                     <div class="card-body text-center mt-4">
-                                        <!-- <h4 class="card-title font-in">Sunlimetech</h4>
-                                        <p class="card-text font-in">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma </p> -->
+                                        <h4 class="card-title font-in">Aguarde</h4>
+                                        <p class="card-text font-in"></p>
                                         <span style="font-size: 1.7em; color: #11998e;">
                         <i class="fab fa-facebook-square "></i>
                         </span>
@@ -138,7 +138,7 @@
                     </div>
                 </div>
 
-                <div class="col-xs-12 col-sm-6 col-md-4" style="visibility: hidden">
+                <div class="col-sm-6 col-md-4">
                     <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
                         <div class="mainflip">
                             <div class="frontside">
@@ -146,9 +146,9 @@
                                     <div class="card-body text-center">
                                         <img class="card-img-top img-card" src="IMG/card-img3.png">
                                         <div class="card-body">
-                                            <h4 class="card-title font-in">Felipe "Brtt"</h4>
-                                            <h5 class="card-subtitle mb-2 font-in">Pro-Player / Youtuber</h5>
-                                            <h5 class="card-subtitle mb-2 font-in">120.754. Seguidores</h6>
+                                            <h4 class="card-title font-in">Ébano</h4>
+                                            <h5 class="card-subtitle mb-2 font-in">Marca</h5>
+                                            <h5 class="card-subtitle mb-2 font-in">124 Seguidores</h6>
                                                 <span style="font-size: 1.7em; color: #11998e;">
                         <i class="fas fa-angle-double-right"></i>
                         </span>
