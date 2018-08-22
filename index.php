5df19e9e69915b241dcee0ecf3e597d6ed4573b5
BiaHer <biaher.oliveira@gmail.com>
2018-08-22 17:09:02 -0300

    banner ebano

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
 
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img class="img2" src="IMG/banner-luiz.png" alt="Ricky-boring">
+                <img class="img2" src="IMG/banner-campanha-ebano.png" alt="Ébano">
                 <div class="carousel-caption mb-5 d-none d-block d-md-block">
                 </div>
             </div>
